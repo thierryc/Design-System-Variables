901e52edc349eaba7a6436494cae5098e927cb36
Thierry Charbonnel <thierryc@users.noreply.github.com>
2019-01-05 17:47:25 -0500

    add long title

--- gh-pages/src/App.js
+++ gh-pages/src/App.js
@@ -1,143 +1,127 @@
-import React, { Component, Fragment } from 'react'
-import * as dsv from './design-system-variables';
-import { H1, H2, H3, H4, H5, H6, P } from './components/Heading'
+import React, { Component, Fragment } from "react";
+import * as dsv from "./design-system-variables";
+import { H1, H2, H3, H4, H5, H6, P } from "./components/Heading";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <div style={{ 
-          
-        }}>
+        <div style={{}}>
+          <P>
+            Le bon sens est la chose du monde la mieux partagée : car chacun
+            pense en être si bien pourvu, que ceux même qui sont les plus
+            difficiles à contenter en toute autre chose, n'ont point coutume
+            d'en désirer plus qu'ils en ont. En quoi il n'est pas vrai-
+            semblable que tous se trompent; mais plutôt cela témoigne que la
+            puissance de bien juger, et distinguer le vrai d'avec le faux, qui
+            est proprement ce qu'on nomme le bon sens ou la raison, est
+            naturellement égale en tous les hommes; et ainsi que la diversité de
+            nos opinions ne vient pas de ce que les uns sont plus raisonnables
+            que les autres, mais seulement de ce que nous conduisons nos pensées
+            par diverses voies, et ne considérons pas les mêmes choses. Car ce
+            n'est pas assez d'avoir l'esprit bon, mais le principal est de
+            l'appliquer bien. Les plus grandes âmes sont capables des plus
+            grands vices, aussi bien que des plus grandes vertus; et ceux qui ne
+            marchent que fort lentement peuvent avancer beaucoup davantage,
+            s'ils suivent toujours le droit chemin, que ne font ceux qui
+            courent, et qui s'en éloignent.
+          </P>
 
-          <P>Le bon sens est la chose du monde la mieux partagée : car chacun pense en être si bien pourvu, 
-            que ceux même qui sont les plus difficiles à contenter en toute autre chose, 
-            n'ont point coutume d'en désirer plus qu'ils en ont. En quoi il n'est pas vrai- semblable que tous se 
-            trompent; mais plutôt cela témoigne que la puissance de bien juger, et distinguer le vrai d'avec 
-            le faux, qui est proprement ce qu'on nomme le bon sens ou la raison, est naturellement égale 
-            en tous les hommes; et ainsi que la diversité de nos opinions ne vient pas de ce que 
-            les uns sont plus raisonnables que les autres, mais seulement de ce que nous conduisons 
-            nos pensées par diverses voies, et ne considérons pas les mêmes choses. 
-            Car ce n'est pas assez d'avoir l'esprit bon, mais le principal est de l'appliquer bien. 
-            Les plus grandes âmes sont capables des plus grands vices, aussi bien que des plus grandes vertus; 
-            et ceux qui ne marchent que fort lentement peuvent avancer beaucoup davantage, s'ils suivent toujours 
-            le droit chemin, que ne font ceux qui courent, et qui s'en éloignent.</P>
-
-          <H1>HOpxl ÀHOpxl ÀHOpxl ÀHOpxl ÀHOpxl À</H1>
-          <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <H1>HO Heading One long Pour moi, je n'ai jamais présumé que mon esprit...</H1>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
           <H2>Variables sub-heading</H2>
-          <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
           <H3>Variables sub-heading</H3>
-          <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
           <H4>Variables sub-heading</H4>
-          <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
-            <H5>Variables sub-heading</H5>
-            <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <H5>Variables sub-heading</H5>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
-            <H6>Variables sub-heading</H6>
-            <P>Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus parfait que ceux du commun; 
-            même j'ai souvent souhaité d'avoir la pensée ou la prompte, ou l'imagination aussi-nette et distincte, 
-            ou la mémoire aussi ample, ou aussi présente, que quelques autres. 
-            Et je ne sache point de qualités que celles-ci, qui servent à la per- fection de l'esprit : 
-            car pour la raison, ou le sens, d'autant qu'elle est la seule chose qui nous rend hommes, 
-            et nous distingue des bêtes, je veux croire qu'elle est tout entière en un chacun, 
-            et suivre en ceci l'opinion commune des philosophes, qui disent qu'il n'y a du plus 
-            et du moins qu'entre les accidents, et non point entre les formes, ou natures, 
-            des individus d'une même espèce.</P>
+          <H6>Variables sub-heading</H6>
+          <P>
+            Pour moi, je n'ai jamais présumé que mon esprit fût en rien plus
+            parfait que ceux du commun; même j'ai souvent souhaité d'avoir la
+            pensée ou la prompte, ou l'imagination aussi-nette et distincte, ou
+            la mémoire aussi ample, ou aussi présente, que quelques autres. Et
+            je ne sache point de qualités que celles-ci, qui servent à la per-
+            fection de l'esprit : car pour la raison, ou le sens, d'autant
+            qu'elle est la seule chose qui nous rend hommes, et nous distingue
+            des bêtes, je veux croire qu'elle est tout entière en un chacun, et
+            suivre en ceci l'opinion commune des philosophes, qui disent qu'il
+            n'y a du plus et du moins qu'entre les accidents, et non point entre
+            les formes, ou natures, des individus d'une même espèce.
+          </P>
 
           <code>
-            <pre>
-html, body 
-	font: 17px/1.6 'Arial';
-
-
-h1 
-	font-size: 4.24em;
-	line-height: 1.13em;
-	padding-top: 0.208em;
-	margin-bottom: 0.167em;
-
-
-h2 
-	font-size: 2.65em;
-	line-height: 1.20em;
-	padding-top: 0.244em;
-	margin-bottom: 0.356em;
-
-
-h3 
-	font-size: 1.65em;
-	line-height: 1.93em;
-	padding-top: 0.607em;
-	margin-bottom: 0.357em;
-
-
-h4 
-	font-size: 1.00em;
-	line-height: 1.59em;
-	padding-top: 0.471em;
-	margin-bottom: 1.12em;
-
-
-p 
-	font-size: 1.00em;
-	line-height: 1.59em;
-	padding-top: 0.471em;
-	margin-bottom: 1.12em;
-
-            </pre>
-
+            <pre>{JSON.stringify(dsv, null, 4)}</pre>
           </code>
-          
-          <code><pre>
-          { JSON.stringify(dsv, null, 4) }
-          </pre></code>
         </div>
       </Fragment>
     );
