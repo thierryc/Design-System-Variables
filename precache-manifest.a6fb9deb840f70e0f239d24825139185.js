95f98d03b039e779309b8b0569c24605405c0a5f
Thierry Charbonnel <thierryc@users.noreply.github.com>
2019-01-05 22:32:43 -0500

    Updates

--- precache-manifest.a6fb9deb840f70e0f239d24825139185.js
+++ precache-manifest.a6fb9deb840f70e0f239d24825139185.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Design-System-Variables/static/js/runtime~main.3a094eef.js"
   },
   {
-    "revision": "1bea0cc8351078412731",
-    "url": "/Design-System-Variables/static/js/main.1bea0cc8.chunk.js"
+    "revision": "eff4a3d33b8fa5db16a4",
+    "url": "/Design-System-Variables/static/js/main.eff4a3d3.chunk.js"
   },
   {
     "revision": "07f58350b57b28a015cf",
     "url": "/Design-System-Variables/static/js/1.07f58350.chunk.js"
   },
   {
-    "revision": "1bea0cc8351078412731",
+    "revision": "eff4a3d33b8fa5db16a4",
     "url": "/Design-System-Variables/static/css/main.466162a7.chunk.css"
   },
   {
-    "revision": "f6af81fdbab1016afc5631d6809b03ad",
+    "revision": "34cbac12e7e80db3f23355ce0d917d2b",
     "url": "/Design-System-Variables/index.html"
   }
 ];
